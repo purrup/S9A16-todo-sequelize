5cfc39d450e2647ca14affd77c54652caa4320fb
Jerry Tsai <purrup27@gmail.com>
2019-05-15 00:19:29 +0800

    add routes and views for register and login pages

--- app.js
+++ app.js
@@ -25,6 +25,30 @@ app.get('/', (req, res) => {
   res.send('hello world!')
 })
 
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+// 註冊檢查
+app.post('/users/login', (req, res) => {
+  res.send('register')
+})
+
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
 app.listen(port, () => {
   console.log(`App is running on localhost:${port}`)
 })
